9a9430c8095a1f2660e7b14e37c9f4ee8ed98b33
BimalShrestha <bimalshrestha1821@gmail.com>
2023-10-06 12:16:42 -0500

    mortgageCalculator

---
+++ Week1Project/src/main/java/org/example/MortgageCalculator.java
@@ -0,0 +1,51 @@
+package org.example;
+import java.util.Scanner;
+public class MortgageCalculator {
+    private double principal;
+    private double annualRate;
+    private int loanYears;
+    private double monthlyPayment;
+    public MortgageCalculator(double principal, double annualRate, int loanYears){
+        this.principal = principal;
+        this.annualRate = annualRate;
+        this.loanYears = loanYears;
+
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the principal: ");
+        double principal = scanner.nextDouble();
+        System.out.println("Enter the Yearly rate: ");
+        double annualRate = scanner.nextDouble();
+        System.out.println("Enter the loan years: ");
+        int loanYears = scanner.nextInt();
+        MortgageCalculator calculator = new MortgageCalculator(principal, annualRate,loanYears );
+        double monthlyPayment = calculator.calculateMonthlyPayment();
+        double totalInterest = calculator.calculateTotalInterest();
+        System.out.printf("Your expected monthly payment is %.2f and you total interest is %.2f",monthlyPayment, totalInterest);
+
+    }
+
+
+    public double getMonthlyInterestRate(){
+        double r = (annualRate/12)/100;
+        return r;
+    }
+    public int getNumberOfPayments(){
+        int n = loanYears*12;
+        return n;
+    }
+    public double calculateMonthlyPayment(){
+        double r = getMonthlyInterestRate();
+        double p = principal;
+        double n = getNumberOfPayments();
+        double m = p *( (r*Math.pow((1+r),n))/(Math.pow((1+r),n)-1));
+        this.monthlyPayment = m;
+        return m;
+    }
+    public double calculateTotalInterest(){
+        double totalInterest = (monthlyPayment*getNumberOfPayments())-principal;
+        return totalInterest;
+    }
+
+}
